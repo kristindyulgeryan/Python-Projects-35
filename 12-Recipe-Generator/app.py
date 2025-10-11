cbebec503c65938cbbc105462b7548c1bd561f63
Kristin <157119345+kristindyulgeryan@users.noreply.github.com>
2025-10-11 18:06:32 +0300

    Update app.py

--- 12-Recipe-Generator/app.py
+++ 12-Recipe-Generator/app.py
@@ -1,1 +1,25 @@
+import random
+
+print("🧑🏼‍🍳 RANDOM RECIPE GENERATOR🧑🏼‍🍳")
+
+proteins = ["chicken", "beef", "tofu", "fish", "eggs"]
+veggies = ["brocoli", "carrots", "spinach", "bell peppers", "mushrooms"]
+carbs = ["rice", "pasta", "potatoes", "quinoa", "bread"]
+methods = ["baked", "grilled", "stir-fried", "roasted", "sauteed"]
+flavors = ["garlic", "lemon", "spicy", "herb", "sweet & sour"]
+
+
+while True:
+    protein = random.choice(proteins)
+    veggie = random.choice(veggies)
+    carb = random.choice(carbs)
+    method = random.choice(methods)
+    flavor = random.choice(flavors)
+
+    print(
+        f"\nYour random recipe: {flavor} {method} {protein} with {veggie} and {carb}")
+
+    if not input("\nGenerate another one? (y/n): ").lower().startswith("y"):
+        print("Goodbye!")
+        break
 
