521e54aff79ab2142b5a6b10f097c5ccee004e07
Kristin <157119345+kristindyulgeryan@users.noreply.github.com>
2026-05-25 19:06:33 +0300

    Create basic Tkinter app with greeting functionality

---
+++ 30-Tkinter-Basic-App/app.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+
+def say_hello():
+    name = name_entry.get()
+
+    if name:
+        greetin_label.config(text=f"Hello, {name}")
+    else:
+        greetin_label.config(text="Hello, World!")
+
+
+window = tk.Tk()
+window.title("My First Tkinter App")
+window.geometry("300x200")
+
+
+window.resizable(False, False)
+
+title_lable = tk.Label(window, text="Welcome to Tkinter!", font=("Arial", 16))
+title_lable.pack(pady=10)
+name_entry = tk.Entry(window, width=20)
+name_entry.pack(pady=10)
+
+hello_button = tk.Button(window, text="Say Hello", command=say_hello)
+hello_button.pack(pady=10)
+greetin_label = tk.Label(window, text="", font=("Arial", "12"))
+greetin_label.pack(pady=10)
+window.mainloop()
